fc4b8a5e3946ee4156fea216f4782834dddb4d0d
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-11-03 00:19:54 +0530

    user model completed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = mongoose.Schema(
   {
-    userName: { type: String, required: true, trim: true },
+    userName: { type: String, required: true, trim: true, unique: true },
     email: {
       type: String,
       required: true,
@@ -56,5 +59,46 @@ const userSchema = mongoose.Schema(
 userSchema.pre("save", async function (next) {
   // only runs if the password gets changed
   if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      role: this.role,
+      userName: this.userName,
+    },
+    process.env.ACCESSTOKEN_SECRET,
+    { expiresIn: "1h" }
+  );
+};
+
+// generate refresh token
+userSchema.methods.generateRefershToken = function () {
+  return jwt.sign({ id: this._id }, process.env.REFRESHTOKEN_SECRET, {
+    expiresIn: "2d",
+  });
+};
+
+// generate the token for password reset and password forget
+userSchema.methods.generateTemporaryToken = function () {
+  const unHahsedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHahsedToken)
+    .digest("hex");
+
+  const min = 20 * 60 * 1000;
+  const tokenExpiry = Date.now() + min;
+  return { unHahsedToken, hashedToken, tokenExpiry };
+};
+
 export const User = mongoose.model("User", userSchema);
