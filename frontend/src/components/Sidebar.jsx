482212179c633978577691a9d46572bf0b34a568
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-12-13 16:57:22 +0530

    .

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Sidebar = function () {
+  return <div>Sidebar</div>;
+};
+export default Sidebar;
--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Sidebar from "./components/Sidebar";
 export default function Layout() {
   return (
     <>
+      <Sidebar />
+      <Navbar />
       <Outlet />
     </>
   );
